0cdaedd16365b488ad047263813a8498b48894e6
Rushi Bhatti <rushibhatti885@gmail.com>
2026-07-08 23:06:22 +0530

    Time: 0 ms (100%), Space: 7.6 MB (88.47%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,6 +4,14 @@ public:
         
         if(n<=1) return n;
 
-        return fib(n-1) + fib(n-2);
+        int last=1 , slast=0 , crunt=0;
+
+        for(int i=2 ; i<=n ; i++){
+            crunt=last+slast;
+            slast=last;
+            last=crunt;
+        }
+
+        return crunt;
     }
 };
