1d59187dff13641f0e6eb82f3c52b19b8cc08232
Rushi Bhatti <rushibhatti885@gmail.com>
2026-07-09 23:00:09 +0530

    Time: 233 ms (1.87%), Space: 29.7 MB (45.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -8,16 +8,12 @@ public:
 
         k = k % n;
 
-        vector<int> arr1 = nums;
-        vector<int> arr2 = nums;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin() , nums.begin()+k);
+        reverse(nums.begin()+k , nums.end());
 
-        arr1.erase(arr1.begin(), arr1.end() - k);
-
-        arr2.erase(arr2.end() - k, arr2.end());
-
-        vector<int> arr = arr1;
-        arr.insert(arr.end(), arr2.begin(), arr2.end());
-
-        nums = arr;
+        for(int i : nums){
+            cout << i <<" ";
+        }
     }
 };
